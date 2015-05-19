a3bdfed17e6a79c04843617803478ecdbe0e6dc8
Nick Flink <nickflink@github.com>
2015-05-19 15:53:17 -0500

    fix the reader and get the collater working but it still needs to wait

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@ fabfile.py used to to run nsq benchmarks
  copyright:  2015, (c) sproutsocial.com
  author:   Nicholas Flink <nicholas@sproutsocial.com>
 """
+import datetime
 import fabric
 import pprint
 import time
@@ -16,12 +17,12 @@ def vagrant():
                                'sub': {'hosts': [], 'key_filename': []},
                                'nsq': {'hosts': [], 'key_filename': []}}
     fabric.api.env.hosts = []
-    fabric.api.env.user = []
+    fabric.api.env.user = "vagrant"
     fabric.api.env.key_filename = []
     for line in status_result.split("\n"):
         vhost = line.split()[0]
         vms.append(vhost)
-    start = time.time()
+    start = datetime.datetime.utcnow()
     for vhost in vms:
         roledef = None
         if vhost.startswith("pub"):
@@ -31,7 +32,8 @@ def vagrant():
         if vhost.startswith("nsq"):
             roledef = 'nsq'
         vhost_addr = None
-        ip_result = fabric.api.local('vagrant ssh '+vhost+' -c "ifconfig eth1"', capture=True)
+        sync_time_hack = "sudo ntpdate time.nist.gov;"
+        ip_result = fabric.api.local('vagrant ssh '+vhost+' -c "'+sync_time_hack+'ifconfig eth1"', capture=True)
         for line in ip_result.split("\n"):
             if line.strip().startswith("inet addr"):
                 vhost_addr = line.strip()[len("inet addr:"):].split()[0]
@@ -40,32 +42,68 @@ def vagrant():
                 if roledef is not None:
                     fabric.api.env.roledefs[roledef]['hosts'].append(vhost_addr)
                     fabric.api.env.roledefs[roledef]['key_filename'].append(id_file)
-                    fabric.api.env.roledefs[roledef]['user'] = 'vagrant'
                 break;
-    end = time.time()
-    ssh_overhead = end - start
-    print "vagrant ssh overhead time:", ssh_overhead
-    pprint.pprint(fabric.api.env.user)
-    pprint.pprint(fabric.api.env.hosts)
-    pprint.pprint(fabric.api.env.key_filename)
+    end = datetime.datetime.utcnow()
+    duration = 10
+    overhead = end - start
+    deadline = end + overhead
+    pprint.pprint(end)
+    pprint.pprint(overhead)
+    pprint.pprint(datetime.timedelta(seconds=duration))
+    DEADLINE_FMT = '%Y-%m-%d %H:%M:%S'
+    fabric.api.env['writer_args'] = [
+        '-batch-size', str(200),
+        '-deadline', "'"+deadline.strftime(DEADLINE_FMT)+"'",
+        '-nsqd-tcp-address', fabric.api.env.roledefs['nsq']['hosts'][0]+":4150",
+        '-runfor', str(duration)+'s',
+        '-size', str(200),
+        '-topic', "sub_bench",
+    ]
+    fabric.api.env['reader_args'] = [
+        '-runfor', str(duration)+'s',
+        '-nsqd-tcp-address', fabric.api.env.roledefs['nsq']['hosts'][0]+":4150",
+        '-size', str(200),
+        '-topic', "sub_bench",
+        '-channel', "ch",
+        '-deadline', "'"+deadline.strftime(DEADLINE_FMT)+"'",
+        '-rdy', str(2500),
+    ]
+    fabric.api.env['collate_args'] = {
+        'wait_until': deadline + datetime.timedelta(seconds=duration),
+    }
+    #print "vagrant ssh overhead time:", overhead
+    #pprint.pprint(fabric.api.env.user)
+    #pprint.pprint(fabric.api.env.hosts)
+    #pprint.pprint(fabric.api.env.key_filename)
 
+@fabric.decorators.parallel
 @fabric.decorators.roles('pub')
 def writer():
     fabric.api.env.hosts = fabric.api.env.roledefs['pub']['hosts']
     fabric.api.env.key_filename = fabric.api.env.roledefs['pub']['key_filename']
-    fabric.api.env.user = fabric.api.env.roledefs['pub']['user']
     nsqd_ip = fabric.api.env.roledefs['nsq']['hosts'][0] 
+    args = " ".join(fabric.api.env['writer_args'])
     # this sleep is really ugly and is not necessary in bash but the run command does not work with out it
-    fabric.api.run("nohup /nsq/bench/bench_writer/bench_writer -nsqd-tcp-address="+nsqd_ip+":4150 &> /tmp/pub.txt < /dev/null & sleep 1")
+    fabric.api.run("nohup /nsq/bench/bench_writer/bench_writer "+args+" &> /tmp/bench.txt < /dev/null &", pty=False)
 
+@fabric.decorators.parallel
 @fabric.decorators.roles('sub')
 def reader():
     #fabric.api.env.hosts = fabric.api.env.roledefs['sub']['hosts']
     #fabric.api.env.key_filename = fabric.api.env.roledefs['sub']['key_filename']
     fabric.api.env.hosts = fabric.api.env.roledefs['sub']['hosts']
     fabric.api.env.key_filename = fabric.api.env.roledefs['sub']['key_filename']
-    fabric.api.env.user = fabric.api.env.roledefs['sub']['user']
-    nsqd_ip = fabric.api.env.roledefs['nsq']['hosts'][0] 
+    args = " ".join(fabric.api.env['reader_args'])
     # this sleep is really ugly and is not necessary in bash but the run command does not work with out it
-    fabric.api.run("nohup /nsq/bench/bench_reader/bench_reader -nsqd-tcp-address="+nsqd_ip+":4150 &> /tmp/sub.txt < /dev/null & sleep 1")
+    fabric.api.run("nohup /nsq/bench/bench_reader/bench_reader "+args+" &> /tmp/bench.txt < /dev/null &", pty=False)
+
+@fabric.decorators.roles('pub', 'sub')
+def collate():
+    wait_until = fabric.api.env['collate_args']['wait_until']
+    wait_seconds = (wait_until - datetime.datetime.utcnow()).total_seconds()
+    print "sleeping %d seconds:" % wait_seconds
+    #time.sleep(wait_seconds)
+    fabric.api.env.hosts = fabric.api.env.roledefs['pub']['hosts'] + fabric.api.env.roledefs['sub']['hosts']
+    fabric.api.env.key_filename = fabric.api.env.roledefs['pub']['key_filename'] + fabric.api.env.roledefs['sub']['key_filename']
+    fabric.api.run("cat /tmp/bench.txt")
 
