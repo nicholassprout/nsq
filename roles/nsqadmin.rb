5f648a32c4711eb97fc7439945189414e45c1f83
Nick Flink <nickflink@github.com>
2015-05-20 08:23:27 -0500

    go up to two pub sub machines each

--- roles/nsqadmin.rb
+++ roles/nsqadmin.rb
@@ -11,11 +11,10 @@ default_attributes(
     }
 )
 
-NSQ_NUM_DAE_HOSTS=1
 override_attributes(
     nsq: {
         nsqadmin: {
-            lookupd_tcp_address: (1..NSQ_NUM_DAE_HOSTS).map{|i| "192.168.138.1#{i}:4160" }
+            lookupd_tcp_address: '172.28.128.3'
         }
     }
 )
--- roles/nsqd.rb
+++ roles/nsqd.rb
@@ -4,11 +4,10 @@ run_list(
     'recipe[nsq::nsqd]'
 )
 
-NSQ_NUM_DAE_HOSTS=1
 override_attributes(
     nsq: {
         nsqd: {
-            lookupd_tcp_address: (1..NSQ_NUM_DAE_HOSTS).map{|i| "192.168.138.1#{i}:4160" }
+            lookupd_tcp_address: '172.28.128.3'
         }
     }
 )
